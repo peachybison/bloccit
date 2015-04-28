e4210dc239def9420ffb6249a7c1226de7548433
JAM <jam212310@gmail.com>
2015-04-28 07:04:31 -0700

    Begin Checkpoint 32 - Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427031909) do
+ActiveRecord::Schema.define(version: 20150428052453) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150427031909) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "questions", force: :cascade do |t|
